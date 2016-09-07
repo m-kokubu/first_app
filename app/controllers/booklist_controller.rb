ab3819c7880e43029d9dff228d7c28f7adf23e9f
kokubu <m-kokubu@froide.co.jp>
2016-09-07 16:33:34 +0900

    #3 メソッド追加

--- app/controllers/booklist_controller.rb
+++ app/controllers/booklist_controller.rb
@@ -8,12 +8,23 @@ class BooklistController < ApplicationController
 
   end
 
+  def create_completed
+    #新規登録完了
+
+  end
+
   def edit
     #編集
 
   end
 
+  def editing_completed
+    #編集完了
+
+  end
+
   def delete
     #削除
+
   end
 end
