5ef74730fd96764414cc2b66eb972e80786bb0e1
kokubu <m-kokubu@froide.co.jp>
2016-09-07 16:13:20 +0900

    #3 各アクション追加

--- app/controllers/booklist_controller.rb
+++ app/controllers/booklist_controller.rb
@@ -1,2 +1,19 @@
 class BooklistController < ApplicationController
+  def index
+    #全件表示
+  end
+
+  def create
+    #新規作成
+
+  end
+
+  def edit
+    #編集
+
+  end
+
+  def delete
+    #削除
+  end
 end
