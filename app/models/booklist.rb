825bbb8bfc10ade825d7bec4dd149e511b85eb05
kokubu <m-kokubu@froide.co.jp>
2016-09-09 15:11:06 +0900

    #9 バリデーション及びエラーメッセージ追加

--- app/models/booklist.rb
+++ app/models/booklist.rb
@@ -1,2 +1,17 @@
 class Booklist < ActiveRecord::Base
+  validates :name, presence: true ,uniqueness: true
+  validates :writer, presence: true ,length:{ maximum: 15}
+
+  # validate :error_sample
+  #
+  # def error_sample
+  #   if name.blank?
+  #     errors[:base] << 'タイトルを入力してください。'
+  #   end
+  #
+  #   if writer.blank?
+  #     errors[:base] << '著者を入力してください。'
+  #   end
+  # end
+
 end
